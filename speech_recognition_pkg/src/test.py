dc689952f105d2eba90bf2da2fc666f44e68b3ea
Tim Müller <mullertim99@gmail.com>
2018-09-14 14:14:41 +0200

    Small test file added to test naoqi import

---
+++ speech_recognition_pkg/src/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# TEST.py
+#
+# A file to test some things in.
+#
+# CHANGELOG: idrc
+#
+
+
+import naoqi
+
+
+def main ():
+    print("What the hey hey")
+
+if __name__ == "__main__":
+    main()
